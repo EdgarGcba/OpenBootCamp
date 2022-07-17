a8af72f34ed1be388ea2cfc6c460aadc9e2baa57
Edgar Galleguillo <edgar.galleguillo@gmail.com>
2022-07-17 20:12:17 -0300

    Unidad 3 Tarea 1

--- Python/Unidad 3_1.py
+++ Python/Unidad 3_1.py
@@ -1,10 +1,4 @@
-Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-nombre = Roxana
-Traceback (most recent call last):
-  File "<pyshell#0>", line 1, in <module>
-    nombre = Roxana
-NameError: name 'Roxana' is not defined
+
 nombre = 'ROxana'
 apellidos = 'Sanchez Martinez'
 edad = 40
