ec36a0352b2287f2dc0f11a40abc6b0f6bbba424
Edgar Galleguillo <edgar.galleguillo@gmail.com>
2022-07-17 20:10:56 -0300

    Unidad 3 tarea 1

---
+++ Python/Unidad 3_1.py
@@ -0,0 +1,16 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+nombre = Roxana
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    nombre = Roxana
+NameError: name 'Roxana' is not defined
+nombre = 'ROxana'
+apellidos = 'Sanchez Martinez'
+edad = 40
+email = 'roxana.sanchez.martinez@gmail.com'
+telefono = '351666333'
+casada = True
+hijos = False
+amigos = ['Vilma', 'Celeste', 'Rosa', 'Paola']
+peliculas = {'Pelicula1': 'Fiebre de sabado', 'Pelicula2': 'Terminator', 'Pelicula3': 'Star War'}
