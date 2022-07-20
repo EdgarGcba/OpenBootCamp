9a31bb775ebe48bbd44c99fd6058948bb727f1d2
Edgar Galleguillo <edgar.galleguillo@gmail.com>
2022-07-20 18:39:42 -0300

    Unidad 7 Tarea 1

---
+++ Unidad 7/Unidad 7_1.py
@@ -0,0 +1,13 @@
+import operaciones as op
+
+suma = op.suma(4, 7)
+print(suma)
+
+resta = op.resta(4, 7)
+print(resta)
+
+multiplicacion = op.multiplicacion(4, 7)
+print(multiplicacion)
+
+division = op.division(4, 7)
+print(division)
---
+++ Unidad 7/operaciones.py
@@ -0,0 +1,11 @@
+def suma (num_a, num_b):
+    return f"{num_a} + {num_b} = {(num_a + num_b)}"
+
+def resta (num_a, num_b):
+    return f"{num_a} - {num_b} = {(num_a - num_b)}"
+
+def multiplicacion (num_a, num_b):
+    return f"{num_a} * {num_b} = {(num_a * num_b)}"
+
+def division (num_a, num_b):
+    return f"{num_a} / {num_b} = {(num_a / num_b)}"
